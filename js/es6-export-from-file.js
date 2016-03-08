49db8e8865a2dca25a7f67a5316bec793e589bbf
John Bartos <jbartos7@gmail.com>
2016-03-07 22:19:58 -0500

    Create es6-export-from-file.js

---
+++ js/es6-export-from-file.js
@@ -0,0 +1,5 @@
+## ES6 export from file
+
+ES6 exports can be used with the `from...` syntax like imports can. Very useful for bringing selectors up and out of the root reducer in Redux.
+
+`export { foo, bar, baz } from './fum'; 
